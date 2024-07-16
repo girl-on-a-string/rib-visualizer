d6e9c7457d62632168724a3940145e6a44280540
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-16 10:33:39 -0500

    added inputs for stitch count and rib width

---
+++ index.js
@@ -0,0 +1,10 @@
+//variables
+
+const mainContainer = document.getElementById("container");
+const interface = document.getElementById("interface");
+const form = document.getElementById("create");
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
+const stitchContainer = document.getElementById("visual");
+
+//
