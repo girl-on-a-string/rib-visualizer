2d2e2b3b9995925a759c3d339cf1f7e7f5186832
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-16 10:53:51 -0500

    created error messages

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 const mainContainer = document.getElementById("container");
 const interface = document.getElementById("interface");
 const form = document.getElementById("create");
-const input = document.getElementById("input");
+
+const stsCountInput = document.getElementById("sts-ct").value; 
+const ribWidthInput = document.getElementById("rib-wdth").value; 
+
 const submitBtn = document.getElementById("submit");
 const stitchContainer = document.getElementById("visual");
 
-//
+//create stitches
+
+
+
+//create display
+
+function createVisual () {
+    if (stsCountInput > 0 && ribWidthInput > 0) {
+        for (let i = 0; i < stsCountInput; i++) {
+            let stitch = document.createElement("div");
+        }
+        console.log(true);
+    } else if (ribWidthInput > stsCountInput) {
+        alert("Your rib width can't be greater than the stitch count");
+    } else {
+        alert("You need to input a value greater than 0 for one or both of your stitch counts");
+        console.log(false);
+    }
+}
